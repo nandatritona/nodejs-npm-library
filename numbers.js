13c1ccb409fc2c98d51c0b267ae1dab5bd4396da
nandatritona <nandhatritona@gmail.com>
2022-05-22 09:00:50 +0700

    Create file

---
+++ numbers.js
@@ -0,0 +1,12 @@
+export const  min = (first, second) => {
+    if (first < second) {
+        return first;
+    }
+    return second;
+}
+export const  max = (first, second) => {
+    if (first > second) {
+        return first;
+    }
+    return second;
+}
